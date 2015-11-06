4a4aba4c0bc26a01013eb0576e73ce58efa10cf8
Alberto Viana <albertogviana@gmail.com>
2015-11-07 00:15:35 +0100

    Implement a simple chat

--- chat.go
+++ chat.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net"
 	"os"
+	"strings"
 )
 
 type Client struct {
 	connection net.Conn
-	channel   chan<- string
+	channel chan string
+	username string
 }
 
 func main() {
@@ -21,6 +25,7 @@ func main() {
 	addChannel := make(chan Client)
 	removeChannel := make(chan Client)
 	messageChannel := make(chan string)
+
 	go handleMessages(messageChannel, addChannel, removeChannel)
 
 	for {
@@ -34,33 +39,33 @@ func main() {
 }
 
 func handleConnection(connection net.Conn, messageChannel chan<- string, addChannel chan<- Client, removeChannel chan<- Client) {
-	buffer := make([]byte, 4096)
-	channel := make(chan string)
+	bufioConnection := bufio.NewReader(connection)
 
-
-	addChannel <- Client{connection, channel}
-
-	defer func() {
-		removeChannel <- Client{connection, channel}
-	}()
-
-	for {
-		n, err := connection.Read(buffer)
-		if err != nil || n == 0 {
-			connection.Close()
-			break
-		}
-
-		messageChannel <-string(buffer[0:n])
-
-		n, err = connection.Write(buffer[0:n])
-		if err != nil {
-			connection.Close()
-			break
-		}
+	client := Client{
+		connection: connection,
+		username: getUsername(connection, bufioConnection),
+		channel: make(chan string),
 	}
 
-	fmt.Printf("Connection from %v closed.\n", connection.RemoteAddr())
+	// Allow a user to quit the chat
+	defer func() {
+		removeChannel <- client
+	}()
+
+	if strings.TrimSpace(client.username) == "" {
+		io.WriteString(connection, "Invalid username\n")
+		connection.Close()
+		return
+	}
+
+	addChannel <- client
+
+	io.WriteString(connection, fmt.Sprintf("Welcome, %s!\n\n", client.username))
+	messageChannel <- fmt.Sprintf("New user %s has joined the chat room.\n", client.username)
+
+	// I/O
+	go client.ReadLines(messageChannel)
+	client.WriteLinesFrom(client.channel)
 }
 
 func handleMessages(messageChannel <- chan string, addChannel <- chan Client, removeChannel <- chan Client) {
@@ -69,15 +74,43 @@ func handleMessages(messageChannel <- chan string, addChannel <- chan Client, re
 	for {
 		select {
 		case message := <- messageChannel:
+			fmt.Printf("%v", message)
 			for _, ch := range clients {
 				go func(mch chan <- string) { mch <- "\033[1;33;40m" + message + "\033[m\r\n" } (ch)
 			}
 		case client := <- addChannel:
+			fmt.Printf("New client: %v \n", client.connection.RemoteAddr())
 			clients[client.connection] = client.channel
-		case conn := <-removeChannel:
-			delete(clients, conn.channel)
+		case client := <- removeChannel:
+			fmt.Printf("Connection from %v closed.\n", client.connection.RemoteAddr())
+			delete(clients, client.connection)
 		}
-//		message := <- messageChannel
-//		fmt.Printf("New message: %s\n", message)
+	}
+}
+
+func getUsername(connection net.Conn, bufioConnection *bufio.Reader) string {
+	io.WriteString(connection, "Welcome to go chat!\n")
+	io.WriteString(connection, "Please, provide your username: ")
+	username, _, _ := bufioConnection.ReadLine()
+	return string(username)
+}
+
+func (client Client) ReadLines(channel chan<- string) {
+	bufioConnection := bufio.NewReader(client.connection)
+	for {
+		line, err := bufioConnection.ReadString('\n')
+		if err != nil {
+			break
+		}
+		channel <- fmt.Sprintf("%s: %s", client.username, line)
+	}
+}
+
+func (client Client) WriteLinesFrom(channel <-chan string) {
+	for message := range channel {
+		_, err := io.WriteString(client.connection, message)
+		if err != nil {
+			return
+		}
 	}
 }
